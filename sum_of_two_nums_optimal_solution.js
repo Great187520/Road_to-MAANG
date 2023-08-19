2d828e9cf23636635efa3e136eb33878aa2cfcdd
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2023-08-19 05:25:53 +0100

    Add files via upload

---
+++ sum_of_two_nums_optimal_solution.js
@@ -0,0 +1,14 @@
+//using Hash Map
+const findTwoSum = function(nums, target){
+    const numsMap = {};
+    for (let p=0; p< nums.length; p++){
+        const currentMapVal = numsMap[nums[p]];
+        if(currentMapVal>= 0){
+            return[currentMapVal,p];
+        }else{
+            const numberToFind = target-nums[p];
+            nums.Map[numberToFind]=p;
+        }
+    }
+    return null;
+}
